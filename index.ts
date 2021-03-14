f5d7254fd4c78c07a8b0f37be7f3c791b701c560
juliantrovato <julian_todobien@hotmail.com>
2021-03-14 10:13:13 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,48 @@
+class Departamento{
+    nombre:string
+    constructor(nombre){
+        this.nombre = nombre
+    }
+    getName(){
+        return this.nombre;
+
+    }
+}
+
+class Piso {
+    nombre:string
+    dptos:Departamento[]
+    constructor(nombre){
+        this.nombre = nombre
+    }
+    pushDepartamento(depto:Departamento){
+       return this.dptos.push(depto);
+
+    }
+    getDepartamento(dpto:Departamento[]){
+        return this.dptos
+
+    }
+
+}
+
+class Edificio {
+  pisos: Piso[];
+  constructor(pisos: Piso[]) {
+    this.pisos = pisos;
+  }
+  addDepartamentoToPiso(nombreDePiso:string, departamento:Departamento){
+      const pisoEncontrado = this.pisos.find(function(item){
+       pisoEncontrado.pushDepartamento(departamento);
+      })
+      
+      
+      }
+      getDepartamentoByPiso(nombreDelPiso:string):Departamento[]{
+          return [];
+
+      }
+
+
+  }
+
